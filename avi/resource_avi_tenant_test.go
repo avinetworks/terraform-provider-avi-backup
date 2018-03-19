5d6a3de7d27462379c8e747bd4788182e71299f2
Rishabh-Baranwal <rishabh.baranwal@avinetworks.com>
2018-03-20 01:04:49 +0530

    1)Corrected tenant test 2)Removed cloud test as it is auto generated 3)Modified GNU make file to run specific test

--- avi/resource_avi_tenant_test.go
+++ avi/resource_avi_tenant_test.go
@@ -2,23 +2,33 @@ package avi
 
 import (
 	"fmt"
-	"testing"
-
+	"github.com/avinetworks/sdk/go/clients"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
+	"strings"
+	"testing"
 )
 
-func TestAVITenantCreate(t *testing.T) {
+func TestAVITenantBasic(t *testing.T) {
+	updatedConfig := fmt.Sprintf(testAccAVITenantConfig, "abc")
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAVITenantDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAVITenantConfig,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAVITenantExists("avi_tenant.test_tenant"),
 					resource.TestCheckResourceAttr(
-						"avi_tenant.test_tenant", "name", "tenant-1")),
+						"avi_tenant.test_tenant", "name", "tenant-%s")),
+			},
+			{
+				Config: updatedConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAVIVSVipExists("avi_tenant.test_tenant"),
+					resource.TestCheckResourceAttr(
+						"avi_tenant.test_tenant", "name", "tenant-abc")),
 			},
 		},
 	})
@@ -27,6 +37,8 @@ func TestAVITenantCreate(t *testing.T) {
 
 func testAccCheckAVITenantExists(resourcename string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
+		conn := testAccProvider.Meta().(*clients.AviClient).AviSession
+		var obj interface{}
 		rs, ok := s.RootModule().Resources[resourcename]
 		if !ok {
 			return fmt.Errorf("Not found: %s", resourcename)
@@ -34,12 +46,44 @@ func testAccCheckAVITenantExists(resourcename string) resource.TestCheckFunc {
 		if rs.Primary.ID == "" {
 			return fmt.Errorf("No AVI Tenant ID is set")
 		}
+		path := "api" + strings.SplitN(rs.Primary.ID, "/api", 2)[1]
+		err := conn.Get(path, &obj)
+		if err != nil {
+			return err
+		}
 		return nil
 	}
 }
 
+func testAccCheckAVITenantDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*clients.AviClient).AviSession
+	var obj interface{}
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "avi_tenant" {
+			continue
+		}
+		path := "api" + strings.SplitN(rs.Primary.ID, "/api", 2)[1]
+		err := conn.Get(path, &obj)
+		if err != nil {
+			if strings.Contains(err.Error(), "404") {
+				return nil
+			}
+			return err
+		}
+		if len(obj.(map[string]interface{})) > 0 {
+			return fmt.Errorf("AVI Tenant still exists")
+		}
+	}
+	return nil
+}
+
 const testAccAVITenantConfig = `
 resource "avi_tenant" "test_tenant"{
-	name= "tenant-1"
+	name= "tenant-%s"
+	config_settings {
+		se_in_provider_context = true
+		tenant_access_to_provider_se = true
+		tenant_vrf = false
+	}
 }
 `
