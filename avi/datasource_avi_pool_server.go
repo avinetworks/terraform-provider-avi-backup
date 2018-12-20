1bbfe5911202d068f248e3eb95d165ce63e6f3f0
Shrikant Chaudhari <shrikant.chaudhari@avinetworks.com>
2018-12-20 12:18:19 +0000

    AV-52945 Updated terraform tempalte 18.2.1. Acceptance test are passed.

--- avi/datasource_avi_pool_server.go
+++ avi/datasource_avi_pool_server.go
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2017. Avi Networks.
+ * Author: Gaurav Rastogi (grastogi@avinetworks.com)
+ *
+ */
 package avi
 
 import "github.com/hashicorp/terraform/helper/schema"
@@ -10,26 +15,22 @@ func dataSourceAviServer() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"ip": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"port": &schema.Schema{
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
-			"type": &schema.Schema{
-				Type:     schema.TypeString,
-				Optional: true,
-				Default:  "V4",
-			},
 			"autoscaling_group_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"availability_zone": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
 			},
 			"description": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
+			},
+			"discovered_networks": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem:     ResourceDiscoveredNetworkSchema(),
 			},
 			"enabled": &schema.Schema{
 				Type:     schema.TypeBool,
@@ -48,6 +49,14 @@ func dataSourceAviServer() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
+			"ip": &schema.Schema{
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem:     ResourceIpAddrSchema(),
+				Set: func(v interface{}) int {
+					return 0
+				},
+			},
 			"location": &schema.Schema{
 				Type:     schema.TypeSet,
 				Optional: true,
@@ -56,15 +65,48 @@ func dataSourceAviServer() *schema.Resource {
 					return 0
 				},
 			},
+			"mac_address": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
 			"nw_ref": &schema.Schema{
 				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"port": &schema.Schema{
+				Type:     schema.TypeInt,
 				Optional: true,
 			},
 			"prst_hdr_val": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
 			},
+			"ratio": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  1,
+			},
+			"resolve_server_by_dns": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
 			"rewrite_host_header": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"server_node": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"static": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"verify_network": &schema.Schema{
 				Type:     schema.TypeBool,
 				Optional: true,
 				Default:  false,
@@ -72,6 +114,7 @@ func dataSourceAviServer() *schema.Resource {
 			"vm_ref": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
+				Computed: true,
 			},
 		},
 	}
--- avi/resource_avi_pool_server.go
+++ avi/resource_avi_pool_server.go
@@ -19,26 +19,22 @@ func ResourceAviPoolServerSchema() map[string]*schema.Schema {
 			Type:     schema.TypeString,
 			Required: true,
 		},
-		"ip": &schema.Schema{
-			Type:     schema.TypeString,
-			Required: true,
-		},
-		"port": &schema.Schema{
-			Type:     schema.TypeInt,
-			Optional: true,
-		},
-		"type": &schema.Schema{
-			Type:     schema.TypeString,
-			Optional: true,
-			Default:  "V4",
-		},
 		"autoscaling_group_name": &schema.Schema{
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		"availability_zone": &schema.Schema{
 			Type:     schema.TypeString,
 			Optional: true,
 		},
 		"description": &schema.Schema{
 			Type:     schema.TypeString,
 			Optional: true,
+		},
+		"discovered_networks": &schema.Schema{
+			Type:     schema.TypeList,
+			Optional: true,
+			Elem:     ResourceDiscoveredNetworkSchema(),
 		},
 		"enabled": &schema.Schema{
 			Type:     schema.TypeBool,
@@ -57,6 +53,14 @@ func ResourceAviPoolServerSchema() map[string]*schema.Schema {
 			Type:     schema.TypeString,
 			Optional: true,
 		},
+		"ip": &schema.Schema{
+			Type:     schema.TypeSet,
+			Required: true,
+			Elem:     ResourceIpAddrSchema(),
+			Set: func(v interface{}) int {
+				return 0
+			},
+		},
 		"location": &schema.Schema{
 			Type:     schema.TypeSet,
 			Optional: true,
@@ -65,16 +69,47 @@ func ResourceAviPoolServerSchema() map[string]*schema.Schema {
 				return 0
 			},
 		},
+		"mac_address": &schema.Schema{
+			Type:     schema.TypeString,
+			Optional: true,
+		},
 		"nw_ref": &schema.Schema{
 			Type:     schema.TypeString,
 			Optional: true,
-			Computed: true,
+		},
+		"port": &schema.Schema{
+			Type:     schema.TypeInt,
+			Optional: true,
 		},
 		"prst_hdr_val": &schema.Schema{
 			Type:     schema.TypeString,
 			Optional: true,
 		},
+		"ratio": &schema.Schema{
+			Type:     schema.TypeInt,
+			Optional: true,
+			Default:  1,
+		},
+		"resolve_server_by_dns": &schema.Schema{
+			Type:     schema.TypeBool,
+			Optional: true,
+			Default:  false,
+		},
 		"rewrite_host_header": &schema.Schema{
+			Type:     schema.TypeBool,
+			Optional: true,
+			Default:  false,
+		},
+		"server_node": &schema.Schema{
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		"static": &schema.Schema{
+			Type:     schema.TypeBool,
+			Optional: true,
+			Default:  false,
+		},
+		"verify_network": &schema.Schema{
 			Type:     schema.TypeBool,
 			Optional: true,
 			Default:  false,
@@ -82,7 +117,6 @@ func ResourceAviPoolServerSchema() map[string]*schema.Schema {
 		"vm_ref": &schema.Schema{
 			Type:     schema.TypeString,
 			Optional: true,
-			Computed: true,
 		},
 	}
 }
