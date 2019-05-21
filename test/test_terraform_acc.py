5b975a73b16f2bc596d14207a18dacb14e7ca86d
rahul chugh <rahulc@avinetworks.com>
2019-05-21 05:43:45 +0000

    AV-63165 test_terraform_acc.py tcid addition

--- test/test_terraform_acc.py
+++ test/test_terraform_acc.py
@@ -5,6 +5,8 @@ logging.basicConfig(filename='terraform_tests.log', level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
 
+
+@pytest.mark.TCID1_48_1607_1_0
 def test_terraform_acc():
     cmd = "make testacc"
     LOG.info("executing command %s ", cmd)
